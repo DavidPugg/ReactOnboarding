411e4e6df64b32058de8a2537b0a99ac7f3b035b
DavidAgiledrop <david.pustoslemsek@agiledrop.com>
2022-03-07 10:40:22 +0100

    Added MainLanguageOption test

--- src/components/atoms/MainLanguageOption/MainLanguageOption.test.tsx
+++ src/components/atoms/MainLanguageOption/MainLanguageOption.test.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
-import LanguageOption from './LanguageOption';
+import MainLanguageOption from './MainLanguageOption';
 
 const myFunc = jest.fn();
 
 it('Check if code is rendered', () => {
-    const { getByText } = render(<LanguageOption label='English' code='en' onClick={myFunc} />);
+    const { getByText } = render(<MainLanguageOption label='English' code='en' onClick={myFunc} />);
     const code = getByText(/en/i);
     expect(code).toBeInTheDocument();
 });
 
 it('Check if label is rendered', () => {
-    const { getByText } = render(<LanguageOption label='English' code='en' onClick={myFunc} />);
+    const { getByText } = render(<MainLanguageOption label='English' code='en' onClick={myFunc} />);
     const label = getByText(/English/i);
     expect(label).toBeInTheDocument();
 });
 
 it('Check if onClick is working', () => {
-    const { getByTestId } = render(<LanguageOption label='English' code='en' onClick={myFunc} />);
-    const el = getByTestId('language-option');
+    const { getByText } = render(<MainLanguageOption label='English' code='en' onClick={myFunc} />);
+    const el = getByText(/English/i);
     fireEvent.click(el);
     expect(myFunc).toBeCalledTimes(1);
 });
