a64d5e3b117e5df139000e07905849223eafabe3
DavidAgiledrop <david.pustoslemsek@agiledrop.com>
2022-03-08 12:28:43 +0100

    LanguageDropdown test code cleanup

--- src/components/molecules/LanguageDropdown/LanguageDropdown.test.tsx
+++ src/components/molecules/LanguageDropdown/LanguageDropdown.test.tsx
@@ -1,40 +1,58 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent,screen } from '@testing-library/react';
 import LanguageDropdown from './LanguageDropdown';
 import { languages } from '../LanguageSwitcher/languages';
 
 const myFunc = jest.fn();
 
-it('test', () => {
-    const { getAllByTestId } = render(
-        <LanguageDropdown
+describe('Check dropdowns', () => {
+    const {getAllByTestId} = screen;
+
+    beforeEach(() => {
+        render(    <LanguageDropdown
             languages={languages}
             currentLanguage={languages[0]}
             fallbackLanguage={languages[1]}
             onClickOutside={myFunc}
             onLanguageChange={myFunc}
-        />,
-    );
-    const buttons = getAllByTestId('main-language-option');
+        />)
+    })
 
-    let dropdowns = getAllByTestId('universal-dropdown');
-    expect(dropdowns).toHaveLength(1);
+    it('Both dropdowns should be closed', () => {
+        let dropdowns = getAllByTestId('universal-dropdown');
+        expect(dropdowns).toHaveLength(1);
+    });
 
-    fireEvent.click(buttons[0]);
-    dropdowns = getAllByTestId('universal-dropdown');
-    expect(dropdowns).toHaveLength(2);
+    it('First dropdown should open', () => {
+        const buttons = getAllByTestId('main-language-option');
+        fireEvent.click(buttons[0]);
+        let dropdowns = getAllByTestId('universal-dropdown');
+        expect(dropdowns).toHaveLength(2);
+    });
 
-    fireEvent.click(buttons[0]);
-    dropdowns = getAllByTestId('universal-dropdown');
-    expect(dropdowns).toHaveLength(1);
+    it('First dropdown should close', () => {
+        const buttons = getAllByTestId('main-language-option');
+        fireEvent.click(buttons[0]);
+        fireEvent.click(buttons[0]);
+        let dropdowns = getAllByTestId('universal-dropdown');
+        expect(dropdowns).toHaveLength(1);
+    });
 
-    fireEvent.click(buttons[0]);
-    fireEvent.click(buttons[1]);
-    dropdowns = getAllByTestId('universal-dropdown');
-    expect(dropdowns).toHaveLength(3);
+    it('Both dropdowns should open', () => {
+        const buttons = getAllByTestId('main-language-option');
+        fireEvent.click(buttons[0]);
+        fireEvent.click(buttons[1]);
+        let dropdowns = getAllByTestId('universal-dropdown');
+        expect(dropdowns).toHaveLength(3);
+    });
 
-    fireEvent.click(buttons[0]);
-    fireEvent.click(buttons[1]);
-    dropdowns = getAllByTestId('universal-dropdown');
-    expect(dropdowns).toHaveLength(1);
+    it('Both dropdowns should close', () => {
+        const buttons = getAllByTestId('main-language-option');
+        fireEvent.click(buttons[0]);
+        fireEvent.click(buttons[1]);
+        fireEvent.click(buttons[0]);
+        fireEvent.click(buttons[1]);
+        let dropdowns = getAllByTestId('universal-dropdown');
+        expect(dropdowns).toHaveLength(1);
+    });
 });
