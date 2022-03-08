1c02d730469b7909de286194345717fa5e603b70
DavidAgiledrop <david.pustoslemsek@agiledrop.com>
2022-03-08 14:07:26 +0100

    Added for search filtering to LanguageDropdown

--- src/components/molecules/LanguageDropdown/LanguageDropdown.test.tsx
+++ src/components/molecules/LanguageDropdown/LanguageDropdown.test.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
-import { render, fireEvent,screen } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import LanguageDropdown from './LanguageDropdown';
 import { languages } from '../LanguageSwitcher/languages';
 
 const myFunc = jest.fn();
 
 describe('Check dropdowns', () => {
-    const {getAllByTestId} = screen;
+    const { getAllByTestId } = screen;
 
     beforeEach(() => {
-        render(    <LanguageDropdown
-            languages={languages}
-            currentLanguage={languages[0]}
-            fallbackLanguage={languages[1]}
-            onClickOutside={myFunc}
-            onLanguageChange={myFunc}
-        />)
-    })
+        render(
+            <LanguageDropdown
+                languages={languages}
+                currentLanguage={languages[0]}
+                fallbackLanguage={languages[1]}
+                onClickOutside={myFunc}
+                onLanguageChange={myFunc}
+            />,
+        );
+    });
 
     it('Both dropdowns should be closed', () => {
         let dropdowns = getAllByTestId('universal-dropdown');
@@ -54,5 +56,29 @@ describe('Check dropdowns', () => {
         fireEvent.click(buttons[1]);
         let dropdowns = getAllByTestId('universal-dropdown');
         expect(dropdowns).toHaveLength(1);
+    });
+});
+
+describe('Check search filtering', () => {
+    it('Should filter correctly', () => {
+        const { getAllByTestId, getByTestId } = render(
+            <LanguageDropdown
+                languages={languages}
+                currentLanguage={languages[0]}
+                fallbackLanguage={languages[1]}
+                onClickOutside={myFunc}
+                onLanguageChange={myFunc}
+            />,
+        );
+        const buttons = getAllByTestId('main-language-option');
+        fireEvent.click(buttons[0]);
+        let options = getAllByTestId('language-option');
+        expect(options).toHaveLength(languages.length - 1);
+
+        const input = getByTestId('language-search');
+        fireEvent.input(input, { target: { value: 'de' } });
+        options = getAllByTestId('language-option');
+        expect(options).toHaveLength(1);
+        expect(options[0]).toHaveTextContent(/de/i);
     });
 });
