65cd54ad1b92d8ccbca99c02d2539e26605e06b1
DavidAgiledrop <david.pustoslemsek@agiledrop.com>
2022-03-08 15:29:14 +0100

    clickOutside test code cleanup

--- src/components/hooks/clickOutside.test.ts
+++ src/components/hooks/clickOutside.test.ts
@@ -2,19 +2,23 @@ import clickOutside from './clickOutside';
 import { fireEvent } from '@testing-library/react';
 
 describe('Test clickOutside', () => {
-    it('On same element click', () => {
+    const setup = () => {
         const myFunc = jest.fn();
         const el = document.createElement('div');
+        return { myFunc, el };
+    };
+
+    it('On same element click', () => {
+        const { el, myFunc } = setup();
         clickOutside({ current: el }, myFunc);
-        fireEvent.mouseDown(el)
+        fireEvent.mouseDown(el);
         expect(myFunc).toBeCalledTimes(0);
     });
 
     it('On body click', () => {
-        const myFunc = jest.fn();
-        const el = document.createElement('div');
+        const { el, myFunc } = setup();
         clickOutside({ current: el }, myFunc);
-        fireEvent.mouseDown(document.body)
+        fireEvent.mouseDown(document.body);
         expect(myFunc).toBeCalledTimes(1);
     });
 });
