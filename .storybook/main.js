28a3c9e4901d3475e4087e267c1bc4b83cd906b4
DavidAgiledrop <david.pustoslemsek@agiledrop.com>
2022-03-10 12:22:43 +0100

    Code cleanup

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,7 @@ const path = require('path');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
-    stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+    stories: ['../src/**/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
     addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
     framework: '@storybook/react',
     webpackFinal: async (config) => {
--- src/components/organisms/Popular/Popular.tsx
+++ src/components/organisms/Popular/Popular.tsx
@@ -1,6 +1,6 @@
 import Toggler from '@components/molecules/Toggler';
 import { Movie, Tv } from 'interfaces/Movies';
-import React, { MutableRefObject, RefObject, useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { PopularSearchResponse } from '../../../utils/MoviesAPI';
 import MovieItem from '../../molecules/MovieItem/MovieItem';
 import styles from './Popular.module.scss';
@@ -13,9 +13,6 @@ export default ({ dataSrc }: Props) => {
     const [items, setItems] = useState([] as Array<any>);
     const [type, setType] = useState('movie');
 
-    const toggleRefOne = useRef<HTMLDivElement>(null);
-    const toggleRefTwo = useRef<HTMLDivElement>(null);
-
     useEffect(() => {
         handleItems('tv');
     }, []);
@@ -24,11 +21,6 @@ export default ({ dataSrc }: Props) => {
         const res = await dataSrc(type);
         setItems(res.results);
         setType(type);
-    };
-
-    const getPosition = () => {
-        if (type == 'tv') return { left: '0%', width: `${toggleRefOne.current?.clientWidth}px` };
-        return { left: '50%', width: `${toggleRefTwo.current?.clientWidth}px` };
     };
 
     return (
