60b412c984c9f2a10415bb2a03a45e7cde3f3a61
DavidAgiledrop <david.pustoslemsek@agiledrop.com>
2022-03-11 09:44:57 +0100

    Updated MainButton to have multiple variations

--- src/components/atoms/MainButton/index.tsx
+++ src/components/atoms/MainButton/index.tsx
@@ -1,21 +1,22 @@
-import React from "react";
-import classNames from "classnames";
+import React from 'react';
+import classNames from 'classnames';
 
-import styles from "./MainButton.module.scss";
+import styles from './MainButton.module.scss';
 
 type Props = {
-  label: string;
-  updated: boolean;
-  onClick: () => void;
+    label: string;
+    updated: boolean;
+    type: string;
+    onClick: () => void;
 };
 
-export default function SearchButton({ label, updated, onClick }: Props) {
-  return (
-    <button
-      onClick={() => onClick()}
-      className={classNames(styles.button, updated ? styles.updated : "")}
-    >
-      {label}
-    </button>
-  );
+export default function SearchButton({ label, updated, onClick, type }: Props) {
+    return (
+        <button
+            onClick={() => onClick()}
+            className={classNames(styles.button, updated ? [styles.updated, styles[`${type}`]] : '')}
+        >
+            {label}
+        </button>
+    );
 }
