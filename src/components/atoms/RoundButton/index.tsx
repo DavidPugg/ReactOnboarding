d9874c2915a427e7de1394cdb85134f401d40666
DavidAgiledrop <david.pustoslemsek@agiledrop.com>
2022-03-14 14:50:16 +0100

    Fixed RoundButton svg import

--- src/components/atoms/RoundButton/index.tsx
+++ src/components/atoms/RoundButton/index.tsx
@@ -10,7 +10,7 @@ export default function RoundButton({ icon, text }: Props) {
     return (
         <div className={styles.item}>
             <svg className={styles.svg}>
-                <use href={require(`@assets/svgs.svg`) + `#icon-${icon}`}></use>
+                <use href={`/svgs.svg#icon-${icon}`}></use>
             </svg>
             <p className={styles.text}>{text}</p>
         </div>
