6d1759354aed763d682975edae33f308433692bc
DavidAgiledrop <david.pustoslemsek@agiledrop.com>
2022-03-15 11:59:43 +0100

    Added 404 and 500 error pages

---
+++ pages/404/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import styles from './404.module.scss'
+
+export default () => {
+    return <h1 className={styles.title}>404 - Page not found :(</h1>
+}
---
+++ pages/500/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import styles from './500.module.scss';
+
+export default () => {
+    return <h1 className={styles.title}>500 - Server-side error occurred :(</h1>;
+};
